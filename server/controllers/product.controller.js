84a07ce1ecaf47c04b48dee39cadc4649f37164a
kimsour-pc <kimsour-pc@users.noreply.github.com>
2026-06-05 09:14:39 +0700

    Log images in product list/get responses for debugging

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -4,6 +4,11 @@ const listProducts = async (req, res, next) => {
   try {
     const filters = req.query;
     const products = await productService.listProducts(filters);
+    try {
+      console.info('List products response sample images:', (products.items && products.items[0] && products.items[0].images) ? products.items[0].images.slice(0, 3) : []);
+    } catch (e) {
+      console.info('List products logging failed', e && e.message);
+    }
     res.json({ success: true, data: products });
   } catch (error) {
     next(error);
@@ -13,6 +18,11 @@ const listProducts = async (req, res, next) => {
 const getProduct = async (req, res, next) => {
   try {
     const product = await productService.getProductById(req.params.id);
+    try {
+      console.info('Get product response images:', product.images && product.images.slice ? product.images.slice(0, 5) : product.images);
+    } catch (e) {
+      console.info('Get product logging failed', e && e.message);
+    }
     res.json({ success: true, data: product });
   } catch (error) {
     next(error);
