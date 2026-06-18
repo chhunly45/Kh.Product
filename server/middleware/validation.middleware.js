29b1bf712844818cd80b513df9e3e441227d23a2
kimsour-pc <kimsour-pc@users.noreply.github.com>
2026-06-18 12:40:16 +0700

    Debug validation errors

--- server/middleware/validation.middleware.js
+++ server/middleware/validation.middleware.js
@@ -2,13 +2,17 @@ const { validationResult } = require('express-validator');
 
 const validate = (req, res, next) => {
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
+    console.log('VALIDATION ERROR:', errors.array());
+
     return res.status(400).json({
       success: false,
       message: 'Validation failed',
-      errors: errors.array().map((error) => ({ field: error.param, message: error.msg }))
+      errors: errors.array()
     });
   }
+
   next();
 };
 
